533561dfb615b878873853fadd87ad6e7f85257a
santhu3092008-glitch <santhu3092008@gmail.com>
2026-05-29 13:08:24 +0530

    Update pythonprogram.py

--- pythonprogram.py
+++ pythonprogram.py
@@ -1,2 +1,8 @@
 def linear_search(list,n,key)
            
+#Simple interest (si) = (principle amount(p) * time(t) *rate)(r)) /100
+p=int(input("Enter the principle amount:")) 
+t=int(inpu("Enter the time in year:")) 
+r=float(input("Enter the rate:")) 
+Si=(p*t*r) /100
+Print("Simple interest is :", Si)
